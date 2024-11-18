963d13cb5d1b0fabcf9efb51cb6eb694be139605
Tony <125552595+ThatCodebyTony@users.noreply.github.com>
2024-11-18 15:58:25 -0800

    Update Node.cpp

--- Node.cpp
+++ Node.cpp
@@ -1,10 +1,11 @@
 // Node.cpp
 #include "Node.h"
-using namespace std;
+
+Node::Node() : word(""), next(nullptr), prev(nullptr) {}
 
 Node::Node(const string& w) : word(w), next(nullptr), prev(nullptr) {}
 
-string Node::getWord() {
+string Node::getWord() const {
     return word;
 }
 
@@ -16,10 +17,92 @@ void Node::setPrev(Node* p) {
     prev = p;
 }
 
-Node* Node::getNext() {
+Node* Node::getNext() const {
     return next;
 }
 
-Node* Node::getPrev() {
+Node* Node::getPrev() const {
     return prev;
+}
+
+void Node::setWord(const string& newWord) {
+    word = newWord;
+}
+
+
+// Non-member operator overloads
+
+// Node compared to Node
+bool operator==(const Node& lhs, const Node& rhs) {
+    return lhs.getWord() == rhs.getWord();
+}
+
+bool operator!=(const Node& lhs, const Node& rhs) {
+    return lhs.getWord() != rhs.getWord();
+}
+
+bool operator<(const Node& lhs, const Node& rhs) {
+    return lhs.getWord() < rhs.getWord();
+}
+
+bool operator>(const Node& lhs, const Node& rhs) {
+    return lhs.getWord() > rhs.getWord();
+}
+
+bool operator<=(const Node& lhs, const Node& rhs) {
+    return lhs.getWord() <= rhs.getWord();
+}
+
+bool operator>=(const Node& lhs, const Node& rhs) {
+    return lhs.getWord() >= rhs.getWord();
+}
+
+// Node compared to string
+bool operator==(const Node& lhs, const string& rhs) {
+    return lhs.getWord() == rhs;
+}
+
+bool operator!=(const Node& lhs, const string& rhs) {
+    return lhs.getWord() != rhs;
+}
+
+bool operator<(const Node& lhs, const string& rhs) {
+    return lhs.getWord() < rhs;
+}
+
+bool operator>(const Node& lhs, const string& rhs) {
+    return lhs.getWord() > rhs;
+}
+
+bool operator<=(const Node& lhs, const string& rhs) {
+    return lhs.getWord() <= rhs;
+}
+
+bool operator>=(const Node& lhs, const string& rhs) {
+    return lhs.getWord() >= rhs;
+}
+
+// String compared to Node
+bool operator==(const string& lhs, const Node& rhs) {
+    return lhs == rhs.getWord();
+}
+
+bool operator!=(const string& lhs, const Node& rhs) {
+    return lhs != rhs.getWord();
+}
+
+bool operator<(const string& lhs, const Node& rhs) {
+    return lhs < rhs.getWord();
+}
+
+bool operator>(const string& lhs, const Node& rhs) {
+    return lhs > rhs.getWord();
+}
+
+bool operator<=(const string& lhs, const Node& rhs) {
+    return lhs <= rhs.getWord();
+}
+
+bool operator>=(const string& lhs, const Node& rhs) {
+    return lhs >= rhs.getWord();
 }
