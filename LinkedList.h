6aa1c8c5acc1f9f9871835958be0e36374224f3d
Tony <125552595+ThatCodebyTony@users.noreply.github.com>
2024-11-18 16:29:04 -0800

    Update LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -7,18 +7,6 @@
 
 using namespace std;
 
-struct LinkedList {
-private:
-    Node* head;
-    Node* tail;#ifndef LINKEDLIST_H
-#define LINKEDLIST_H
-
-#include "Node.h"
-#include <string>
-#include <iostream>
-
-using namespace std;
-
 class LinkedList {
 private:
     Node* head;
@@ -54,31 +42,6 @@ public:
 };
 
 // Function declaration for external use
-Node* findWord(LinkedList* lDict, string userWord);
-
-#endif
-
-    int listSize;
-
-    void setHead(Node* h);
-    void setTail(Node* t);
-    void setSize(int s);
-
-public:
-    LinkedList();
-    LinkedList(const string& wordInfo);
-    ~LinkedList();
-    void push_back(const string& word);
-    void clear();
-    int size();
-    void print(ostream& os = cout);
-    bool linearSearch(const string& word, string& prevWord, string& nextWord);
-
-    Node* getHead() const;
-    Node* getTail() const;
-};
-
-// Function declaration
 Node* findWord(LinkedList* lDict, string userWord);
 
 #endif
