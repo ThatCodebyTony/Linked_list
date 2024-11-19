f4fe48b92e5ec28cd1b928c82742de4675b328f6
Tony <125552595+ThatCodebyTony@users.noreply.github.com>
2024-11-18 16:28:19 -0800

    Update LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -10,7 +10,54 @@ using namespace std;
 struct LinkedList {
 private:
     Node* head;
+    Node* tail;#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include "Node.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class LinkedList {
+private:
+    Node* head;
     Node* tail;
+    int listSize;
+
+    void setHead(Node* h);
+    void setTail(Node* t);
+    void setSize(int s);
+
+public:
+    LinkedList();
+    LinkedList(const string& wordInfo);
+    ~LinkedList();
+    
+    LinkedList& operator+=(LinkedList& otherList);
+
+
+    void push_back(const string& word);
+    void push_back(Node * newWordNode);
+    void clear();
+    int size();
+    void print(ostream& os = cout);
+    Node* linearSearch(const string& word, string& prevWord, string& nextWord);
+    Node* deleteWord(Node* nodeToDelete);
+    Node* insert_before(string newWord, Node* knownNode);
+
+    // Declare the insert_in_order method
+    void insert_in_order(const string& newWord);
+
+    Node* getHead() const;
+    Node* getTail() const;
+};
+
+// Function declaration for external use
+Node* findWord(LinkedList* lDict, string userWord);
+
+#endif
+
     int listSize;
 
     void setHead(Node* h);
